70b5e55fdccdfcb4c04013e4780eeece203c206c
imzwq <876867644@qq.com>
2020-09-01 22:53:36 +0800

    发布订阅者模式

--- src/App.js
+++ src/App.js
@@ -1,45 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { PureComponent } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
-function Index () {
-  useEffect(() => {
-    console.log('useEffect=>您来了Index页面')
-    // return () => {
-    //   console.log('你离开了Index页面')
-    // }
-  }, [])
-  return <h2>Index</h2>
-}
-
-function List () {
-  useEffect(() => {
-    console.log('useEffect=>您来了List页面')
-  })
-  return <h2>List</h2>
-}
-
-export default function App () {
-  const [ count, setCount ] = useState(0)
-  useEffect(() => {
-    console.log(`useEffect=>You click ${count} times`)
-    return () => {
-      console.log('======')
-    }
-  }, [])
-  return (
-    <div>
-      <p>You click {count} times</p>
-      <button onClick={() => {setCount(count + 1)}}>Click me</button>
-
+export default class App extends PureComponent {
+  
+  render () {
+    return (
       <Router>
         <ul>
           <li><Link to='/'>首页</Link></li>
           <li><Link to='/List'>列表</Link></li>
         </ul>
 
-        <Route path="/" exact component={Index}></Route>
-        <Route path="/list" component={List}></Route>
+        {/* <Route path="/" exact component={}></Route>
+        <Route path="/list" component={}></Route> */}
       </Router>
-    </div>
-  )
+    )
+  }
 }
